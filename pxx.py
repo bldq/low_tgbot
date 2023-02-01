3456759b70f8846fb802a2f0e9e904571735491e
wangwaner <13401843622@163.com>
2023-02-01 13:47:47 +0000

    1.0

--- pxx.py
+++ pxx.py
@@ -6,47 +6,26 @@ from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHan
 # import re
 # import json
 import os
-import xmlrpc.client
-from random import sample
 from telegram import InputMediaPhoto
 import get_url
 import config
+import traverse_img
 # s = xmlrpc.client.ServerProxy('http://127.0.0.1:6800/rpc')
 
-# s.aria2.addUri('token:Aria21281066939',['http://example.org/file'],dict(dir="/tmp"))
-def add_download_rpc(url_list,path):
-    # jsonreq = json.dumps({'jsonrpc':'2.0', 'id':'qwer', 'method':'aria2.addUri',
-    #                      'params':['token:Aria21281066939', [link],{}]})
-    # requests.post('http://127.0.0.1:6800/jsonrpc', jsonreq)
-    s = xmlrpc.client.ServerProxy('http://127.0.0.1:6801/rpc')
-    for url in url_list: 
+# # s.aria2.addUri('token:Aria21281066939',['http://example.org/file'],dict(dir="/tmp"))
+# def add_download_rpc(url_list,path):
+#     # jsonreq = json.dumps({'jsonrpc':'2.0', 'id':'qwer', 'method':'aria2.addUri',
+#     #                      'params':['token:Aria21281066939', [link],{}]})
+#     # requests.post('http://127.0.0.1:6800/jsonrpc', jsonreq)
+#     s = xmlrpc.client.ServerProxy('http://127.0.0.1:6801/rpc')
+#     for url in url_list: 
         
-        s.aria2.addUri('token:Aria21281066939',[url],dict(dir=os.path.join(r"/downloads",path)))
+#         s.aria2.addUri('token:Aria21281066939',[url],dict(dir=os.path.join(r"/downloads",path)))
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-
-# def get_url(url):
-#     headers = {
-#                 "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
-#                 }
-#     response = requests.get(url,headers=headers)
-#     # response = requests.get(url,headers=headers,proxies=proxies)
-#     # 获取响应的 html 内容
-#     html = response.content.decode('utf-8')
-#     url_list = re.findall('ess-data=\'([a-zA-z]+://[^\s]*)\'', html)
-#     # print('共获得%d个链接'%len(url_list))
-#     soup = BeautifulSoup(html,'html.parser')
-#     path = soup.h4.string
-#     # print('开始导入链接')
-#     #down_path = 'C:\\Users\\王莞尔\\Desktop\\xiaocao\\'+ str(path)
-#     down_path = str(path)
-#     add_download_rpc(url_list, down_path)
-#     # for url in url_list:
-#     #     add_download_rpc(url_list)
-#     return "从该网址获得{many}张图片地址， 题名为:{title}".format(many=str(len(url_list)), title=path)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please sent me 1024 link")
@@ -65,23 +44,31 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_message(chat_id=update.effective_chat.id, text='Not 1024 link')
 
 async def show_at_random(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    dirpath = r'/root/docker_aria2-pro/downloads_local' 
-    img_list = from_item = []
-    for root, dirs, files in os.walk(dirpath):
-        for file in files:
-            full_name = os.path.join(root, file)
-            img_list.append(full_name)
-            from_item.append(os.path.basename(full_name))
+    # dirpath = r'/root/docker_aria2-pro/downloads_local' 
+    # img_list = from_item = []
+    # for root, dirs, files in os.walk(dirpath):
+    #     for file in files:
+    #         full_name = os.path.join(root, file)
+    #         img_list.append(full_name)
+    #         from_item.append(os.path.basename(full_name))
     
-    result = sample(img_list,9)
+    # result = sample(img_list,9)
     pic_list = []
+    result = traverse_img.rows_nub()
     for pic in result:
-        pic_list.append(InputMediaPhoto(open(pic, 'rb'), caption = 'enjoy'))
+        pic_list.append(InputMediaPhoto(open(pic[1], 'rb'), caption = pic[2]))
     await context.bot.send_media_group(chat_id=update.effective_chat.id, media=pic_list)
         # await context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(pic, 'rb'))
 
-async def list_img_db(update: Update, context: ContextTypes.DEFAULT_TYPE)
-    pass
+async def list_img_db(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_chat.id not in [1905615710, 910241302]:
+        logging.info('-'.join((str(update.effective_chat.id),'unauthed_one')))
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='此功能仅限管理员')
+    try:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=traverse_img.reset_media_db())
+    except ValueError:
+        pass
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(config.BOT_TOKEN).build()
@@ -92,4 +79,5 @@ if __name__ == '__main__':
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
     application.add_handler(send_1024_pic)
+    application.add_handler(list_img)
     application.run_polling()
--- traverse_img.py
+++ traverse_img.py
@@ -1,33 +1,44 @@
 import sqlite3
 import os
 import config
-from random import sample
-def list():
-    dirpath = r'/root/docker_aria2-pro/downloads_local' 
-    img_list = from_item = []
-    for root, dirs, files in os.walk(dirpath):
-        for file in files:
-            full_name = os.path.join(root, file)
-            img_list.append(full_name)
-            from_item.append(os.path.basename(full_name))
-    
-    result = sample(img_list,9)
-    pic_list = []
+import random
+# import imghdr
+
+# def list(dirpath):
+#     # dirpath = r'/root/docker_aria2-pro/downloads_local' 
+#     img_list = []
+#     from_item = []
+#     size_list = []
+#     for root, dirs, files in os.walk(dirpath):
+#         for file in files:
+#             full_name = os.path.join(root, file)
+#             # print(full_name)
+#             img_list.append(full_name)
+#             from_item.append(os.path.basename(os.path.dirname(full_name)))
+#             size_list.append(os.path.getsize(full_name))
+#             imgType = imghdr.what(full_name)
+#             # print(from_item)
+#             # exit()
+#             # print(len(img_list))
+#     return (img_list, from_item, size_list)
+    # result = sample(img_list,9)
+    # pic_list = []
     # for pic in result:
         # pic_list.append(InputMediaPhoto(open(pic, 'rb'), caption = 'enjoy'))
         # from_item.
     # await context.bot.send_media_group(chat_id=update.effective_chat.id, media=pic_list)
         # await context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(pic, 'rb'))
 
-def create_db(db_path):
-    conn = sqlite3.connect(db_path)
+def create_db():
+    
+    conn = sqlite3.connect(config.DB_PATH)
     try:
         create_tb_cmd = '''
             CREATE TABLE IF NOT EXISTS media
             (
             MEDIA_ID INT,
-            MEDIA_NAME TEXT,
-            MEDIA_DIR TEXT,
+            MEDIA_FULL_PATH TEXT,
+            MEDIA_ITEM TEXT,
             MEDIA_SIZE INT);
             '''
         conn.execute(create_tb_cmd)
@@ -37,47 +48,64 @@ def create_db(db_path):
     conn.close()
 
 
-def reset_media_db(db_path):
-    conn = sqlite3.connect(db_path)
-    c = conn.cursor()
-    c.execute("DROP TABLE media")
+def reset_media_db():
+    conn = sqlite3.connect(config.DB_PATH)
+    c = conn.cursor() 
+    c.execute("DROP TABLE IF EXISTS media")
     create_tb_cmd = '''
             CREATE TABLE media
             (
-            MEDIA_ID INT,
-            MEDIA_NAME TEXT,
-            MEDIA_DIR TEXT,
+            MEDIA_ID primary key,
+            MEDIA_FULL_PATH TEXT,
+            MEDIA_ITEM TEXT,
             MEDIA_SIZE INT);
             '''
     c.execute(create_tb_cmd)
     # c.execute('''CREATE TABLE book(BOOK_ID INT, BOOK_NAME TEXT,  BOOK_DIR TEXT)''')
-    i = 0
-    # print(Config.book_root_dir)
-    for (dirname, dirs, files) in os.walk(config.ROOT_DIR):
-        for filename in files:
-            if filename.endswith(('.txt', '.epub', '.mobi', '.pdf')):
-            # if filename.endswith(('.txt', '.epub', '.mobi')):
-                i = i+1
-                full_path = os.path.join(dirname, filename)
-                # print(type(i))
-                book_c = (i, filename, full_path)
-                # print (os.path.getsize(thefile), thefile)
-                # print(str(i), thefile)
-                c.execute("insert into book values (?, ?, ?)", book_c)
+    # dirpath = r'/root/docker_aria2-pro/downloads_local' 
+    # img_list = []
+    # from_item = []
+    # size_list = []
+    n = 0
+    for root, dirs, files in os.walk(config.ROOT_DIR):
+        for file in files:
+            full_name = os.path.join(root, file)
+            item = os.path.basename(os.path.dirname(full_name))
+            size = os.path.getsize(full_name)
+            if int(size) < 10000000:
+                n += 1
+            # print(n)
+                c.execute("insert into media values (?, ?, ?, ?)", (n, full_name, item, size))
+    conn.commit()
+    create_tb_cmd = '''
+            CREATE TABLE IF NOT EXISTS nub
+            (
+            MEDIA_NUB int);
+            '''
+    c.execute(create_tb_cmd)
+    c.execute("insert into nub values (?)", (n,))
     conn.commit()
     conn.close()
     # print('共获得%d条图书信息' % i)
-    return i
+    return '共获得%d条有效图片信息' % n
 
 
 def rows_nub():
-    conn = sqlite3.connect(db_path)
+    conn = sqlite3.connect(config.DB_PATH)
     c = conn.cursor()
-    c.execute("SELECT count(*) FROM book")
-    hangshu = c.fetchall()
-    rdm_nub = random.randint(1, int(hangshu[0][0]))
-    c.execute("SELECT * FROM book where BOOK_ID=?", (rdm_nub,))
+    # c.execute("SELECT count(*) FROM nub")
+    # hangshu = c.fetchall()
+    c.execute("SELECT * FROM media")
+    # hangshu = c.fetchall()
+    # rdm_nub = random.randint(1, int(hangshu[0][0]))
+    # c.execute("SELECT * FROM media where MEDIA_SIZE<?", (10000000,))
     values = c.fetchall()
+    result = random.sample(values,9)
     conn.commit()
     conn.close()
-    return values
+    return result
+
+if __name__ == '__main__':
+    # create_db()
+    # reset_media_db()
+    print(reset_media_db())
