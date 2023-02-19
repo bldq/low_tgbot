3e055e7c1468d756ba8b17d7647f376ac1db7a03
wangwaner <13401843622@163.com>
2023-02-19 09:56:17 +0000

    清除了一些无用文件

--- 1024_crawler/test2.py
+++
@@ -1,5 +0,0 @@
-import xmlrpc
-import xmlrpc.client
-s = xmlrpc.client.ServerProxy('http://127.0.0.1:6800/rpc')
-
-s.aria2.addUri('token:Aria21281066939',['http://example.org/file'],dict(dir="/tmp"))
--- config.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = '5790080134:AAFgk-Njer82TZnaRq6cSVf43EuewRnBHnI'
-ROOT_DIR = r'/root/docker_aria2-pro/downloads_local'
-RPC_SERVER = r'http://127.0.0.1:6801/rpc'
-# ‘token:’后面的才是要填的rpctoken
-RPC_TOKEN = 'token:Aria21281066939'
-DB_PATH = '/root/tg_bot/1024_crawler/img.db'
